ff347ecdf6f3e0ba0b34196a55bb50674558dfbf
Jens Pentenrieder <jens.pentenrieder@hpe.com>
2019-02-06 11:14:09 -0700

    add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,65 +1,31 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import { Box, Text } from "grommet";
 
 const Navbar = class extends React.Component {
+  render() {
+    return (
+      <Box>
+        <Box>
+          <Link to="/about">
+            <Text>About</Text>
+          </Link>
+        </Box>
 
-  componentDidMount() {
-    // Get all "navbar-burger" elements
-   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    // Check if there are any navbar burgers
-   if ($navbarBurgers.length > 0) {
- 
-     // Add a click event on each of them
-     $navbarBurgers.forEach( el => {
-       el.addEventListener('click', () => {
- 
-         // Get the target from the "data-target" attribute
-         const target = el.dataset.target;
-         const $target = document.getElementById(target);
- 
-         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-         el.classList.toggle('is-active');
-         $target.classList.toggle('is-active');
- 
-       });
-     });
-   }
- }
- 
- render() {
-   return (
-  
-  <nav className="navbar is-transparent" role="navigation" aria-label="main-navigation">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-        </Link>
-        {/* Hamburger menu */}
-        <div className="navbar-burger burger" data-target="navMenu">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-      <div id="navMenu" className="navbar-menu">
-      <div className="navbar-start has-text-centered">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
-      </div>
-      </div>
-    </div>
-  </nav>
-  )}
-}
+        <Box>
+          <Link to="/contact">
+            <Text>Contact</Text>
+          </Link>
+        </Box>
 
-export default Navbar
+        <Box>
+          <Link to="/contact/examples">
+            <Text>Examples</Text>
+          </Link>
+        </Box>
+      </Box>
+    );
+  }
+};
+
+export default Navbar;
